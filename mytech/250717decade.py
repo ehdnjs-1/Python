4cebda298660156ea13b41e64a0ef3383a36f3a4
dowon <dowon@gmail.com>
2025-07-20 22:41:43 +0900

    at home

--- mytech/250717decade.py
+++ mytech/250717decade.py
@@ -2,22 +2,26 @@ import sys
 import itertools
 import getpass
 
-user_pass = getpass.getpass("패스워드를 입력하시오: ")  # 화면에 표시되지 않음
+real_password = "dowon"  # 암호를 미리 지정
+
 password = [chr(i) for i in range(ord('a'), ord('z')+1)] + [str(i) for i in range(0, 10)]  # 알파벳+숫자
 
-for guess_tuple in itertools.product(password, repeat=len(user_pass)):
-    guess = ''.join(guess_tuple)
-    if guess == user_pass:
-        break  # 반복문만 종료
+print("패스워드 분석 중...")  # 분석 시작 안내
 
-# 아래에 보안 프로그램 코드 추가 (최대 3번 시도)
-real_password = user_pass  # 위에서 입력받은 패스워드를 실제 암호로 사용
+found_password = None
+for guess_tuple in itertools.product(password, repeat=len(real_password)):
+    guess = ''.join(guess_tuple)
+    print(f"암호해제 시도 중: {guess}")  # 현재 시도 중인 패스워드 출력
+    if guess == real_password:
+        found_password = guess
+        break  # 찾은 값 저장 후 반복문 종료
 
 max_attempts = 3
 for attempt in range(max_attempts):
     user_input = getpass.getpass("암호를 입력하세요: ")
     if user_input == real_password:
         print("환영합니다 도원님")
+        print(f"분석된 암호: {found_password}")  # 분석된 암호 보여주기
         break
     else:
         print("접근 거부")
